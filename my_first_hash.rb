0309979a2e969dbe0921a62ab8099118a1c51a4d
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-29 13:05:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash(key, value)
+def my_hash(your_hash)
   your_hash = {"name"=> "Michael", "birthday"=> "November 22, 1988"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
